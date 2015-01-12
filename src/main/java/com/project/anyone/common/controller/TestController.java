049ef380726f3ca1537c94981a025414d6b293b7
heesu <ruvieye@naver.com>
2015-01-12 13:12:32 +0900

    * formatter 적용 * jstl pom xml 추가

--- src/main/java/com/project/anyone/common/controller/TestController.java
+++ src/main/java/com/project/anyone/common/controller/TestController.java
@@ -13,28 +13,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
-	@Autowired
-	private SqlSession sqlSession;
-
-	@RequestMapping(value = "/hello",  method = RequestMethod.GET)
-	public String hello(Locale locale, Model model) {
-
-		HashMap<String, String> input = new HashMap<String, String>();
-		List<HashMap<String, String>> outputs = sqlSession.selectList(
-				"com.project.anyone.selectTestUserList", input);
-		System.out.print(outputs.toString());
-
-		return "hello";
-	}
-
-	@RequestMapping(value="/insert")
-	public String insert(){
-		HashMap<String, String> input = new HashMap<String, String>();
-		List<HashMap<String, String>> outputs = sqlSession.selectList(
-				"com.project.anyone.insertTestUser", input);
-		System.out.print(outputs.toString());
-
-		return "hello";
-	}
-	
+  @Autowired
+  private SqlSession sqlSession;
+  
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
+  public String hello(Locale locale, Model model) {
+    
+    HashMap<String, String> input = new HashMap<String, String>();
+    List<HashMap<String, String>> outputs = sqlSession.selectList("com.project.anyone.selectTestUserList", input);
+    System.out.print(outputs.toString());
+    
+    return "hello";
+  }
+  
+  @RequestMapping(value = "/insert")
+  public String insert() {
+    HashMap<String, String> input = new HashMap<String, String>();
+    List<HashMap<String, String>> outputs = sqlSession.selectList("com.project.anyone.insertTestUser", input);
+    System.out.print(outputs.toString());
+    
+    return "hello";
+  }
+  
 }
