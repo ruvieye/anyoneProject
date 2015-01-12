77a5122adede1a77d9b3dbc61641fd712a088966
heesu <ruvieye@naver.com>
2015-01-12 10:41:05 +0900

    테스트 rebase

--- src/main/java/com/project/anyone/common/controller/TestController.java
+++ src/main/java/com/project/anyone/common/controller/TestController.java
@@ -4,6 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 public class TestController {
 	@Autowired
