68a9afebbd7a2677948b85fb27491110103ce842
blackeyeys <younsik.kim_88@nhnent.com>
2015-01-12 10:29:43 +0900

    테스트 커밋

--- src/main/java/com/project/anyone/common/controller/TestController.java
+++ src/main/java/com/project/anyone/common/controller/TestController.java
@@ -4,19 +4,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 @Controller
 public class TestController {
 	@Autowired
 	private SqlSession sqlSession;
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@RequestMapping(value = "/hello",  method = RequestMethod.GET)
 	public String hello(Locale locale, Model model) {
 
 		HashMap<String, String> input = new HashMap<String, String>();
